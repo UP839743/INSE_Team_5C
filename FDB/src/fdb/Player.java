c057870847f3fc6b649200e3d6f1ba8515dbf671
Samuel Reeve <sreeve96@googlemail.com>
2018-02-19 18:05:53 +0000

    Begun adding classes with basic get methods for Staff.Java
    TODO - player, stadium, manager, trophy getter's,

---
+++ FDB/src/fdb/Player.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fdb;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Player extends Staff{
+    int goalsScored;
+
+}
---
+++ FDB/src/fdb/Staff.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fdb;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Staff {
+    String staffFirstName;
+    String staffLastName;
+    String position;
+    Date DoB;
+    String nationality;
+    
+    
+    private String getFirstName(String query){
+    System.out.println("Connection attempted");
+    try{  
+    Class.forName("com.mysql.jdbc.Driver");  
+    Connection con=DriverManager.getConnection("jdbc:mysql://localhost/homes","INSE","root");  
+    Statement stmt=con.createStatement();  
+    ResultSet rs=stmt.executeQuery("select * from Player where " + query);  
+    while(rs.next())  
+        System.out.println(rs.getString(1));  
+        con.close();  }
+    catch(Exception e){
+        System.out.println(e);
+        System.out.println("Connection Failed");
+    }
+    return staffFirstName;
+    }
+    
+    private String getLastName(){
+    return staffLastName;
+    }
+    
+    private String getPosition(){
+    return position;
+    }
+    
+    private Date getDoB(){
+    return DoB;
+    }
+    
+    private String getNationality(){
+    return nationality;
+    }
+}
