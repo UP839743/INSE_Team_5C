092dfb37506d6e5d941198d39499b2ebf4c1f3d9
UP839743 <up839743@myport.ac.uk>
2018-03-10 13:18:00 +0000

    Assed database connector

--- FDB/src/fdb/FXMLDocumentController.java
+++ FDB/src/fdb/FXMLDocumentController.java
@@ -5,23 +5,120 @@
  */
 package fdb;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  *
  * @author INSE Team 5C
  */
+
+
 public class FXMLDocumentController implements Initializable {
     
     @FXML
+<<<<<<< HEAD
     private Label label;
     
+=======
+    private Button btnChelsea;
+    @FXML
+    private Button btnArsenal;
+    @FXML
+    private Button btnTottenham;
+    @FXML
+    private Button btnManCity;
+    @FXML
+    private Button btnTeamDetails;
+    @FXML
+    private Button btnHome;
+    @FXML
+    private Button btnFixtures;
+    @FXML
+    private Button btnClubDetails;
+    @FXML
+    private Button btnMoreDetails;
+    
+    private final String chelseaThemeUrl = getClass().getResource("css/Chelsea.css").toExternalForm();
+    private final String arsenalThemeUrl = getClass().getResource("css/Arsenal.css").toExternalForm();
+    private final String tottenhamThemeUrl = getClass().getResource("css/Tottenham.css").toExternalForm();
+    private final String mancityThemeUrl = getClass().getResource("css/Man City.css").toExternalForm();
+   
+    private final String arsenalTeamDetailsURL = getClass().getResource("css/ArsenalTeamDetails.css").toExternalForm();
+    
+    private static String stylesheet = "";  
+    private static int team = 0;
+    
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException{
+     Stage stage = null; 
+     Parent root = null;
+     
+     System.out.println("Stylesheet before= "+stylesheet);     
+     if(event.getSource()== btnChelsea || event.getSource()== btnArsenal || event.getSource()== btnTottenham || event.getSource()== btnManCity || event.getSource() == btnHome){
+        
+         if(event.getSource() == btnChelsea){
+           stylesheet = chelseaThemeUrl;  
+         }
+         else if (event.getSource()== btnArsenal){
+           stylesheet = arsenalThemeUrl;
+           team = 1;
+         }
+         else if (event.getSource() == btnTottenham){
+             stylesheet = tottenhamThemeUrl;
+         }
+         else if (event.getSource() == btnManCity){
+             stylesheet = mancityThemeUrl;
+         }
+                 
+        Button btn = (Button) event.getSource(); 
+        stage=(Stage) btn.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        
+     }
+          
+     else if (event.getSource() == btnTeamDetails){
+        stage = (Stage) btnTeamDetails.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("Team Details.fxml"));
+        
+        if (team == 1){
+            stylesheet = arsenalTeamDetailsURL;
+        }
+        
+     }
+     
+     else if (event.getSource() == btnFixtures){
+         stage = (Stage) btnFixtures.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("Fixtures and Results.fxml"));
+       
+     }
+     
+     else if (event.getSource() == btnClubDetails || event.getSource() == btnMoreDetails){
+        stage = (Stage) btnClubDetails.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("Club Details.fxml"));
+        
+     }
+     //create a new scene with root and set the stage
+      Scene scene = new Scene(root);
+      stage.setScene(scene);
+      scene.getStylesheets().add(stylesheet);
+      System.out.println("Stylesheet after= " + stylesheet);
+      stage.show();
+
+    } 
+>>>>>>> UI
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
